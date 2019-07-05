7fa942b8829038106742ec0a3da8b897af5e71ef
dongxiaofeng <1197045900@qq.com>
2019-07-05 17:56:13 +0800

    server-user

---
+++ manage-server/server-user/src/main/java/com/feng/manage/server/user/ServerUserApplicationStarter.java
@@ -0,0 +1,20 @@
+package com.feng.manage.server.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Xiaofeng
+ * @Date: 2019/7/5 17:02
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ServerUserApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerUserApplicationStarter.class, args);
+    }
+
+}
